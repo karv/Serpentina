2d6ee8218236be2f076970e16da662a16038263f
Edgar Carballo <karvayoEdgar@gmail.com>
2015-01-05 15:45:46 -0600

    Se introducen en ArregloCíclico: Clone() y ToArray().

--- Serpentina/ArregloCíclico.cs
+++ Serpentina/ArregloCíclico.cs
@@ -53,28 +53,33 @@ namespace Structs
 			// ArregloCíclico<T> r = new ArregloCíclico<T> ();
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Structs.ArregloCíclico`1"/> class.
+		/// </summary>
+		/// <param name="TamañoMáximo">Tamaño máximo.</param>
+		/// <param name="Inic">Estado inicial del arreglo.</param>
+		public ArregloCíclico (int TamañoMáximo, T[] Inic) : this(TamañoMáximo)
+		{
+			_Data = Inic;
+		}
+
+
 		// IEnumerable <T>
 		public IEnumerator<T> GetEnumerator()
 		{
 			// Volcar _Data en otro T[]
-			T[] tmp = new T[MaxTamaño];
-			for (int i = 0; i < Tamaño; i++) {
-				int j = (i + _Pos) % MaxTamaño;
-				tmp [i] = _Data [j];
-			}
+			T[] tmp = ToArray ();
 			return (IEnumerator<T>)tmp.GetEnumerator ();
 		}
 
 		IEnumerator IEnumerable.GetEnumerator ()
 		{
 			// Volcar _Data en otro T[]
-			T[] tmp = new T[MaxTamaño];
-			for (int i = 0; i < Tamaño; i++) {
-				int j = (i + _Pos) % MaxTamaño;
-				tmp [i] = _Data [j];
-			}
+			T[] tmp = ToArray ();
 			return tmp.GetEnumerator ();
 		}
+
+
 		// Acceso.
 
 		public T this[int i]
@@ -87,6 +92,30 @@ namespace Structs
 				int j = (i + _Pos) % MaxTamaño;
 				_Data [j] = value;
 			}		
+		}
+
+		/// <summary>
+		/// Clona memberwise este objeto.
+		/// </summary>
+		public ArregloCíclico<T> Clone ()
+		{
+			ArregloCíclico<T> ret = new ArregloCíclico<T> (MaxTamaño, _Data);
+			ret._Pos = _Pos;
+			return ret;
+		}
+
+		/// <summary>
+		/// Convierte este arreglo cíclico en un arreglo.
+		/// </summary>
+		/// <returns>The array.</returns>
+		public T[] ToArray()
+		{
+			T[] ret = new T[MaxTamaño];
+			for (int i = 0; i < Tamaño; i++) {
+				int j = (i + _Pos) % MaxTamaño;
+				ret [i] = _Data [j];
+			}
+			return ret;
 		}
 	}
 }
