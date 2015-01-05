4118edc554237517f6b7959cf381a4e2e5c17b17
Edgar Carballo <karvayoEdgar@gmail.com>
2015-01-05 15:25:11 -0600

    Separar en diferentes archivos. Corregida la falta de enumerador.

---
+++ Serpentina/Estructuras.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Structs
+{
+	public struct Par <S, T>
+	{
+		public S x;
+		public T y;
+
+		public override bool Equals (object obj)
+		{
+			if (obj is Par<S, T>) {
+				Par <S, T> cmp = (Par<S, T>)obj;
+				return x.Equals (cmp.x) && y.Equals (cmp.y);
+			}
+			return false;
+		}
+		public override int GetHashCode ()
+		{
+			return base.GetHashCode ();
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("(%0, %0)", x.ToString (), y.ToString ());
+		}
+
+		public Par (S a, T b)
+		{
+			x = a;
+			y = b;
+		}
+	}
+}
+
