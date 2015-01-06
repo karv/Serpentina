b9cf4c91c2e107ca9e0df711572556be872fe856
karv <karvayoEdgar@gmail.com>
2015-01-05 18:29:03 -0600

    + Clase Universo.

---
+++ Serpentina/Universo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Structs;
+
+namespace Serpentina
+{
+    /// <summary>
+    /// Representa el universo de juego.
+    /// </summary>
+    public class Universo
+    {
+        List<Serpiente> _Jugadores = new List<Serpiente>();
+        
+        /// <summary>
+        /// Devuelve la lista de jugadores.
+        /// </summary>
+        public List<Serpiente> Jugadores
+        {
+            get { return _Jugadores; }
+        }
+
+        /// <summary>
+        /// Representa el área del juego.
+        /// </summary>
+        public Rectángulo<int, int> Área;
+
+        /// <summary>
+        /// Ejecuta el juego.
+        /// </summary>
+        public void Run()
+        {
+            while (true)
+            {
+                
+            }
+        }
+        
+    }
+}
