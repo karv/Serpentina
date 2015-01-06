8e9b3372acf08169ea5c04e2e7bc60ba9871aca1
karv <karvayoEdgar@gmail.com>
2015-01-05 18:23:10 -0600

    Class Rectángulo
    Se crea la clase rectángulo, y se arreglan bugs en Par

--- Serpentina/Estructuras.cs
+++ Serpentina/Estructuras.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Structs
 {
-	public struct Par <S, T>
+	public class Par <S, T>
 	{
 		public S x;
 		public T y;
@@ -30,6 +30,34 @@ namespace Structs
 			x = a;
 			y = b;
 		}
+        public Par()
+        {
+        }
 	}
+
+    public class Rectángulo<S, T> : Par<Par<S, T>,Par<S,T>>
+    {
+        public S x1
+        {
+            get { return x.x; }
+        }
+        public S x2
+        {
+            get { return y.x; }
+        }
+        public T y1
+        {
+            get { return x.y; }
+        }
+        public T y2
+        {
+            get { return y.y; }
+        }
+
+        public Rectángulo(S nx1, T ny1, S nx2, T ny2) : base(new Par<S,T>(nx1, ny1), new Par<S,T> (nx2, ny2))
+        {
+
+        }
+    }
 }
 
