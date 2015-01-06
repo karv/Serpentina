b2af74b568412a7f129de816fab0273152152fa5
karv <karvayoEdgar@gmail.com>
2015-01-05 19:17:49 -0600

    Probando

--- Serpentina/Program.cs
+++ Serpentina/Program.cs
@@ -7,7 +7,15 @@ namespace Serpentina
 		public static void Main (string[] args)
 		{
 			Serpiente s = new Serpiente (10);
+            Universo U = new Universo();
+            U.Área = new Structs.RectánguloInt(0, 0, 40, 40);
 
+            s.Posición.x = 3;
+            s.Posición.y = 3;
+
+            U.Jugadores.Add(s);
+
+            U.Run();
 
 			Console.WriteLine ("Hello World!");
 		}
