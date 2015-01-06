c97706e0114d4102a1b7faeb5157ad63aeb9541a
Edgar Carballo <karvayoEdgar@gmail.com>
2015-01-06 11:35:18 -0600

    Interface en consola para Serpiente

--- Serpentina/Program.cs
+++ Serpentina/Program.cs
@@ -6,10 +6,25 @@ namespace Serpentina
 	{
 		public static void Main (string[] args)
 		{
-			Serpiente s = new Serpiente (10);
+			SerpienteConsola s = new SerpienteConsola (10, 10, 10);
+
+			s.MaxLongitud = 7;
+
+			s.Avanzar (Serpiente.enumDirecciónAbsoluta.Derecha);
+			s.Avanzar (Serpiente.enumDirecciónAbsoluta.Izquierda);
+			s.Avanzar (Serpiente.enumDirecciónAbsoluta.Arriba);
+			s.Avanzar (Serpiente.enumDirecciónAbsoluta.Abajo);
+
+			Console.WriteLine (s.Pos (0));
+			Console.WriteLine (s.Pos (1));
+			Console.WriteLine (s.Pos (2));
+			Console.WriteLine (s.Pos (3));
+				
+
+			Console.ReadLine ();
 
 
-			Console.WriteLine ("Hello World!");
+
 		}
 	}
 }
---
+++ Serpentina/SerpienteConsola.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Serpentina
+{
+	/// <summary>
+	/// Agrega la posibilidad de dibujarse en consola a <see cref="Serpentina.Serpiente"/> .
+	/// </summary>
+	public class SerpienteConsola:Serpiente
+	{
+		public const char bg = ' ';
+		public const char fg = 'x';
+		public override void Avanzar (enumDirecciónAbsoluta dir)
+		{
+			Structs.Par<int, int> PosTmp = Pos (Longitud - 1); 	// Obtiene la posición de la cola.
+			Console.CursorLeft = PosTmp.x;
+			Console.CursorTop = PosTmp.y;
+			Console.Write (bg);
+
+			base.Avanzar (dir);
+
+			PosTmp = Pos (0); 	// Obtiene la posición de la cabeza
+
+			Console.CursorLeft = PosTmp.x;
+			Console.CursorTop = PosTmp.y;
+			Console.Write (fg);
+
+
+		}
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Serpentina.SerpienteConsola"/> class.
+		/// </summary>
+		/// <param name="MaxTamaño">Tamaño máximo (por memoria).</param>
+		/// <param name="x">Posición x inicial</param>
+		/// <param name="y">Posición y inicial</param>
+		public SerpienteConsola (int MaxTamaño, int x, int y):base(MaxTamaño,x,y) {}
+			
+
+	}
+}
+
