b6e45fe27935a02f828025e48628422d61248301
Edgar Carballo <karvayoEdgar@gmail.com>
2017-07-19 13:03:30 -0700

    Setup the UI

--- Serpentina/Program.cs
+++ Serpentina/Program.cs
@@ -1,11 +1,28 @@
 using System;
+using CE.Console;
+using CE.Console.Controls;
 
 namespace Serpentina
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		public static App App;
+		static KeyComb _escapeKey;
+		public static void Main ()
 		{
+			var design = CE.Console.Controls.Import.JsonImporter.ImportFromFile ("GameUI.json");
+			_escapeKey = new KeyComb (ConsoleKey.Escape);
+			App = new App (design: design);
+			App.Initialize ();
+			var kl = App.GetSystem<KeyboardListener> ();
+			kl.KeyPressed += KeyPressed;
+			App.Run ();
+		}
+
+		static void KeyPressed (object sender, KeyEventArgs e)
+		{
+			if (_escapeKey.IsInstance (e.Key))
+				App.Exit ();
 		}
 	}
 }
